3fe1eabe3b4d209bc7f567d75731d83eadc45ae4
Nlucky320 <luckyes144@gmail.com>
2024-05-10 22:09:50 +0600

    7th commit

--- src/Pages/Viewdetails/ViewDetails.jsx
+++ src/Pages/Viewdetails/ViewDetails.jsx
@@ -4,11 +4,62 @@ import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 import { Helmet } from "react-helmet-async";
-
+import Modal from 'react-modal';
+import useAuth from "../../Hooks/useAuth";
+const customStyles = {
+  content: {
+    top: '57%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 
 const ViewDetails = () => {
- 
-    const foodsItem = useLoaderData();
+   let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const foodsItem = useLoaderData();
+  const initialData = useLoaderData();
+  const [item, setItem] = useState(initialData);
+  const { user } = useAuth();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    if (user) {
+      setFormData({
+        name: user.displayName || "",
+        email: user.email || "",
+        photo: user.photoURL || "",
+      });
+    }
+  }, [user]);
+  useEffect(() => {
+    setItem(initialData);
+  }, [initialData]);
+  const handleRequest= (event) => {
+    event.preventDefault();
+      
+  };
     return (
          <div
       key={foodsItem._id}
@@ -64,12 +115,204 @@ const ViewDetails = () => {
           </p>
                 </div>
                 <div className="text-center">
-                      <button className="font-bold animate__animated animate__pulse animate__infinite  rounded-md px-4 py-2 bg-[#f9a06f] text-[#1e1b4b] relative overflow-hidden group z-0 hover:text-[#1e1b4b] duration-1000">
+                      <button onClick={openModal} className="font-bold animate__animated animate__pulse animate__infinite  rounded-md px-4 py-2 bg-[#f9a06f] text-[#1e1b4b] relative overflow-hidden group z-0 hover:text-[#1e1b4b] duration-1000">
                   <span className="absolute bg-[#ffede2]  size-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
                   <span className="absolute bg-[#fac0a1] size-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
                   Request
-                </button>
-              </div>
+            </button>
+            
+          </div>
+           <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+              contentLabel="Example Modal"
+              
+      >
+              <div className=" flex items-end justify-end">
+                  <button  onClick={closeModal} className="font-bold animate__animated animate__pulse animate__infinite  rounded-md px-4 py-2 bg-[#f9a06f] text-[#1e1b4b] relative overflow-hidden group z-0 hover:text-[#1e1b4b] duration-1000">
+                <span className="absolute bg-[#ffede2]  size-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+                  <span className="absolute bg-[#fac0a1] size-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>close</button>
+            </div>
+   
+        <form onSubmit={handleRequest}>
+          {/* form food_name and food_image row */}
+          <div className="md:flex mb-2">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Food Name</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="food_name"
+                                    placeholder="Food Name"
+                                    defaultValue={item?.food_name}
+                        className="input input-bordered w-full"
+                           readOnly
+                />
+              </label>
+            </div>
+            <div className="form-control md:w-1/2 md:ml-4">
+              <label className="label">
+                <span className="label-text">Food Image</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="food_image"
+                                    placeholder="Food Image"
+                                     defaultValue={item?.food_image}
+                        className="input input-bordered w-full"
+                           readOnly
+                />
+              </label>
+            </div>
+          </div>
+          {/* form food quantity and pickup location row */}
+          <div className="md:flex mb-2">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Food Quantity</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="food_quantity"
+                                    placeholder="Food Quantity"
+                                     defaultValue={item?.food_quantity}
+                        className="input input-bordered w-full"
+                           readOnly
+                />
+              </label>
+            </div>
+            <div className="form-control md:w-1/2 md:ml-4">
+              <label className="label">
+                <span className="label-text">Pickup Location</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="pickup_location"
+                                    placeholder="Pickup Location"
+                                        defaultValue={item?.pickup_location}
+                        className="input input-bordered w-full"
+                           readOnly
+                />
+              </label>
+            </div>
+          </div>
+          {/* form expired_date and food_status row */}
+          <div className="md:flex mb-2">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Expired Date</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="expired_date"
+                                    placeholder="Expired Date"
+                                    defaultValue={item?.expired_date}
+                        className="input input-bordered w-full"
+                           readOnly
+                />
+              </label>
+            </div>
+            <div className="form-control md:w-1/2 md:ml-4">
+              <label className="label">
+                <span className="label-text">Food Status</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="food_status"
+                  placeholder="Food Status"
+                  defaultValue={item?.food_status}
+                        className="input input-bordered w-full"
+                           readOnly
+                />
+              </label>
+            </div>
+          </div>
+          {/* form additional_notes  row */}
+          <div className="md:flex mb-2">
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text">Additional notes</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="additional_notes"
+                                    placeholder="Additional notes"
+                                    defaultValue={item?.additional_notes}
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+          {/* form name and email row */}
+          <div className="md:flex mb-2">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="displayName"
+                  placeholder="Name"
+                  value={formData.name}
+                  className="input input-bordered w-full"
+                  readOnly
+                />
+              </label>
+            </div>
+            <div className="form-control md:w-1/2 md:ml-4">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  className="input input-bordered w-full"
+                  readOnly
+                />
+              </label>
+            </div>
+          </div>
+          {/* form Photo url row */}
+          <div className="mb-2">
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Image URL</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="image"
+                  value={formData.photo}
+                  placeholder="Image URL"
+                  className="input input-bordered w-full"
+                  readOnly
+                />
+              </label>
+            </div>
+          </div>
+          <input
+            type="submit"
+            value="Request"
+            className="btn border-none btn-block bg-black text-white"
+          />
+        </form>
+      </Modal>
+    </div>
             </div>
            
     </div>
